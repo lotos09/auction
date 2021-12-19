a00a41d9266b6b7a40853fc0ab144d68755d0681
lotos09 <41050548+lotos09@users.noreply.github.com>
2021-12-19 20:06:22 +0300

    made chat with firebase

---
+++ src/components/adminPanel/navigation/AppRouter.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { CHAT_ROUTE, MAIN_PAGE_ROUTE, MANAGE_CONTENT_ROUTE } from '../../../untils/constants';
+import { Routes, Route } from 'react-router-dom';
+import { Chat } from '../app/chat/Chat';
+import { Context } from '../../../index';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { MainPage } from '../../MainPage';
+import { ManageContentPage } from '../app/manageContent/ManageContentPage';
+import { Navbar } from './Navbar';
+import { NotFound } from './NotFound';
+
+const AppRouter = () => {
+  const { auth } = useContext(Context);
+  const [user] = useAuthState(auth);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index key={MAIN_PAGE_ROUTE} element={<MainPage />} />
+          {!!user && (
+            <Route key={MANAGE_CONTENT_ROUTE} path="content" element={<ManageContentPage />} />
+          )}
+          {!!user && <Route key={CHAT_ROUTE} path="chat" element={<Chat />} />}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default AppRouter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import firebase from 'firebase';
+import 'firebase/storage';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat';
+import 'firebase/firestore';
+import 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBzFNuZpZFmo00ODltBPhHPVDiiIC64GLE",
+//     authDomain: "auction-1459b.firebaseapp.com",
+//     projectId: "auction-1459b",
+//     storageBucket: "auction-1459b.appspot.com",
+//     messagingSenderId: "692768848863",
+//     appId: "1:692768848863:web:e3a01dfbe362ea17d8a26a"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+export const myBase = firebase.initializeApp({
+  apiKey: 'AIzaSyBzFNuZpZFmo00ODltBPhHPVDiiIC64GLE',
+  authDomain: 'auction-1459b.firebaseapp.com',
+  projectId: 'auction-1459b',
+  storageBucket: 'auction-1459b.appspot.com',
+  messagingSenderId: '692768848863',
+  appId: '1:692768848863:web:e3a01dfbe362ea17d8a26a',
+});
+
+export const Context = createContext(null);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider
+      value={{
+        firebase,
+        auth,
+        firestore,
+      }}
+    >
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
