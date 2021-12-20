7a3059d645b392f2d0486f9c51993e439d65e3c3
lotos09 <41050548+lotos09@users.noreply.github.com>
2021-12-20 13:42:10 +0300

    start making gallery

--- src/components/adminPanel/app/manageContent/ManageContentPage.js
+++ src/components/adminPanel/app/manageContent/ManageContentPage.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import { myBase } from '../../../../index';
+import React, { useEffect, useState } from 'react';
+import { myBase } from '../../../../firebase/config';
+
+const db = myBase.firestore();
 
 export const ManageContentPage = () => {
-  const onFileChange = async e => {
+
+  const [fileUrl, setFileUrl] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  const onFileChange = async (e) => {
     const file = e.target.files[0];
     const storageRef = myBase.storage().ref();
     const fileRef = storageRef.child(file.name);
     await fileRef.put(file);
+    setFileUrl(await fileRef.getDownloadURL());
   };
 
-  const onSubmit = e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log('asd')
+    const username = e.target.username.value;
+    if (!username || !fileUrl) {
+      return;
+    }
+    await db.collection("users").doc(username).set({
+      name: username,
+      avatar: fileUrl,
+    });
   };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersCollection = await db.collection("users").get();
+      setUsers(
+        usersCollection.docs.map((doc) => {
+          return doc.data();
+        })
+      );
+    };
+    fetchUsers();
+  }, []);
 
   return (
     <>
-      <form>
+      <form onSubmit={onSubmit}>
         <input type="file" onChange={onFileChange} />
-        <input type="text" name="username" placeholder="name" />
-        <input type="submit" value="Submit" onSubmit={onSubmit} />
+        <input type="text" name="username" placeholder="NAME" />
+        <button>Submit</button>
       </form>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.name}>
+              <img width="100" height="100" src={user.avatar} alt={user.name} />
+              <p>{user.name}</p>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/compat';
+
+export const myBase = firebase.initializeApp({
+  apiKey: 'AIzaSyBzFNuZpZFmo00ODltBPhHPVDiiIC64GLE',
+  authDomain: 'auction-1459b.firebaseapp.com',
+  projectId: 'auction-1459b',
+  storageBucket: 'auction-1459b.appspot.com',
+  messagingSenderId: '692768848863',
+  appId: '1:692768848863:web:e3a01dfbe362ea17d8a26a',
+});
